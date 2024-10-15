49cc7354f4b20e47d0ac3ae9599015292153f20e
Tamil-janani <selvamtamilarasan6@gmail.com>
2024-10-15 17:07:02 +0530

    Add files via upload

---
+++ tasttlow.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ int main()
+ {
+     int a,b,c,d,mod,n;
+     scanf("%d%d%d",&a,&b,&c);
+     d=a+b,
+     n=a+c;
+     mod=d-n;
+     printf("%d\n",mod);
+ }
+
+
---
+++ testtthard.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ int main ()
+ {
+     int x,y;
+     printf("Enter the two Number:\n");
+     scanf("%d%d",&x,&y);
+     printf("sum of %d",x*y);
+     return 0 ;
+}
+
+
